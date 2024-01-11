1b55fae8d048b9b40529b8f0a9b349591d3c9db2
s27085 <s27085@pjwstk.edu.pl>
2024-01-11 03:41:02 +0100

    database connection added

--- projekt/source/js/app.js
+++ projekt/source/js/app.js
@@ -1,24 +1,14 @@
 import 'bootstrap';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, get, child } from "firebase/database";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {Database, Flashcard} from './firebaseDB.js';
+let db1 = new Database();
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyD-L5w-UCh4FwDU7BvQ1gc-tF7PRziPhgQ",
-    authDomain: "flashcards-5c554.firebaseapp.com",
-    projectId: "flashcards-5c554",
-    storageBucket: "flashcards-5c554.appspot.com",
-    messagingSenderId: "553502189588",
-    appId: "1:553502189588:web:6ba6745ed0a2a6171b9f01",
-    measurementId: "G-WDVSZG7CMP"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const analytics = getAnalytics(app);
+$("#add_flashcard").submit ((e) => {
+    e.preventDefault();
+    console.log("test");
+    let flashcard = {
+        question: $("#question").value,
+        answer: $("#answer").value
+    }
+    console.log(flashcard);
+    return false;
+});
